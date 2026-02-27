1436289a7a9f5d988f639fa3d8e5b4ba5605cac3
Dhanshreeshende <dhanshreeshende030@gmail.com>
2026-02-27 08:12:29 +0530

    feat: add basic KNN visualization component structure

---
+++ KNNViz.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const KNNViz = () => {
+  return (
+    <div>
+      <h2>K-Nearest Neighbors Visualization</h2>
+      <p>This module demonstrates KNN classification.</p>
+    </div>
+  );
+};
+
+export default KNNViz;
+
+
+
+feat: add basic KNN visualization component structure
